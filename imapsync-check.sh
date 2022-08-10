9491ef0d251b7c9ed39dfec1cc2e1a63e3e2ed50
Ashley Cawley <ash@ashleycawley.co.uk>
2022-08-10 12:46:37 +0100

    Update imapsync-check.sh
    Added a report at the end

--- imapsync-check.sh
+++ imapsync-check.sh
@@ -9,8 +9,11 @@ NC='\033[0m' # No Color
 SAVEIFS=$IFS    # Backing up the delimiter used by arrays to differentiate between different data in the array (prior to changing it)
 IFS=$'\n'       # Changing the delimiter used by arrays from a space to a new line, this allows a list of users (on new lines) to be stored in to an array
 
+EMAIL_LIST_FOR_REPORT=()
+
 for EMAIL_MIGRATION in $(cat sync-list.txt)
 do
+        EMAIL_ADDRESS=$(echo "$EMAIL_MIGRATION" | awk '{print $5}')
         echo "$EMAIL_MIGRATION" | sh
 
         if [ `echo $?` != 0 ]
@@ -20,8 +23,19 @@ do
                 ${RED}$EMAIL_MIGRATION${NC}
 
                 Please rectify the issue so that it can proceed with the rest..."
+                EMAIL_LIST_FOR_REPORT+=("ERROR - $EMAIL_ADDRESS")
+
+                echo "Report:"
+                printf '%s\n' "${EMAIL_LIST_FOR_REPORT[@]}"
                 exit 1
+        else
+                EMAIL_LIST_FOR_REPORT+=("OK - $EMAIL_ADDRESS")
         fi
 done
 
+echo -e "\n ### Migration Summary ###\n"
+printf '%s\n' "${EMAIL_LIST_FOR_REPORT[@]}"
+
 IFS=$SAVEIFS # Resets $IFS this changes the delimiter that arrays use from new lines (\n) back to just spaces (which is what it normally is)
+
+exit 0
